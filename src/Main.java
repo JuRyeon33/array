fa87e7dd20e72724744eda00b3ba317af096a6bd
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-09 11:36:41 +0900

    반대로 출력

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
 
+        for (int i = 6; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
     }
 }
